6e81e9bcaa2bfe520888bcbca348a20454873b1b
diego856 <diegomarco@hotmail.com.ar>
2021-11-15 22:50:38 -0300

    agregando forms

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3030
 
 const publicPath = path.join(__dirname, "/public")
 app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname,'/views/login.html'))
 })
 
 app.listen(port, () => {
