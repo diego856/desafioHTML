c0182088c68c48d530c22cf29cb64f4bfd80511b
diego856 <diegomarco@hotmail.com.ar>
2021-11-17 19:26:29 -0300

    mejorando el camion

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3030
+const port = 3000
 
 const publicPath = path.join(__dirname, "/public")
 app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'/views/home.html'))
-})
-
-app.post('/', (req, res) => {
   res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
@@ -20,6 +16,10 @@ app.get('/register', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname,'/views/login.html'))
+})
+
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
 app.listen(port, () => {
